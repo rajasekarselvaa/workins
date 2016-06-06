53a8c9dbb7fd2ffd411c24969476cab060df84b7
Rajasekar Selvam <rajasekar.embedded@gmail.com>
2016-06-06 14:48:40 +0530

    u-boot of imx6ul_evk board

---
+++ mx6ul_14x14_evk_config/include/config.h
@@ -0,0 +1,9 @@
+/* Automatically generated - do not edit */
+#define CONFIG_IMX_CONFIG	board/freescale/mx6ul_14x14_evk/imximage.cfg
+#define CONFIG_MX6UL	1
+#define CONFIG_BOARDDIR board/freescale/mx6ul_14x14_evk
+#include <config_defaults.h>
+#include <config_uncmd_spl.h>
+#include <configs/mx6ul_14x14_evk.h>
+#include <asm/config.h>
+#include <config_fallbacks.h>
